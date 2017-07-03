d3a334b8d0bd2e330957dd23aebff59917af53fe
Mdjurdj1 <mdjurdj1@gmail.com>
2017-07-03 03:01:37 -0400

    SeedDataFactory converted to single line

--- app/core/services/seed-data.factory.js
+++ app/core/services/seed-data.factory.js
@@ -1,9 +1,3 @@
 // This is a seed data factory service to be used with angular resource. Currently unused.
 
-angular.module('app').factory('SeedDataFactory', function ($resource) {
-
-    return $resource('core/data/:filename.json', {
-      filename: '@filename'
-    });
-
-  });
+angular.module('app').factory('SeedDataFactory', function ($resource) {});
